58c08ff4318fbace0a202cd8058d5051140ae899
Naveen -TG,🇮🇳 [#offline] <93958628+Naveen-TG@users.noreply.github.com>
2025-02-20 13:55:29 +0530

    Update index.py

--- plugins/index.py
+++ plugins/index.py
@@ -1,5 +1,5 @@
 import logging
-import asyncio
+import asyncio # lol 
 from pyrogram import Client, filters, enums
 from pyrogram.errors import FloodWait
 from pyrogram.errors.exceptions.bad_request_400 import ChannelInvalid, ChatAdminRequired, UsernameInvalid, UsernameNotModified
