38e45f950849dfba0ee54c1eb7003b961748aaca
Naveen -TG,🇮🇳 [#offline] <93958628+Naveen-TG@users.noreply.github.com>
2025-02-21 08:54:01 +0530

    Update route.py

--- plugins/route.py
+++ plugins/route.py
@@ -1,7 +1,45 @@
+WEB_URL = "https://hot-lucita-jiraiya-cd68c555.koyeb.app/"
+WEB_SLLEP = 3*60
+
 from aiohttp import web
+import asyncio, aiohttp, logging, traceback
+
+
+
+log = logging.getLogger(__name__)
+
 
 routes = web.RouteTableDef()
 
-@routes.get("/", allow_head=True)
-async def root_route_handler(request):
-    return web.json_response("VijayTGx")
+@routes.get('/', allow_head=True)
+async def hello(request):
+    return web.Response(text="Hello, world!")
+
+
+def web_server():
+    app = web.Application()
+    app.add_routes(routes)
+    return app
+
+
+
+
+async def keep_alive():
+    if WEB_URL:
+        while True:
+            await asyncio.sleep(WEB_SLLEP)
+            try:
+                async with aiohttp.ClientSession(
+                    timeout=aiohttp.ClientTimeout(total=10)
+                ) as session:
+                    async with session.get(WEB_URL) as resp:
+                        log.info(
+                            "Pinged {} with response: {}".format(
+                                WEB_URL, resp.status
+                            )
+                        )
+            except asyncio.TimeoutError:
+                log.warning("Couldn't connect to the site URL..!")
+            except Exception:
+                traceback.print_exc()
+                
